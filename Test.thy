60bcdb313793b08bf2419b5d554d4a51fb044d9f
Dominique Unruh <unruh@ut.ee>
2020-02-27 18:41:56 +0200

    Tidied up Test.thy

--- Test.thy
+++ Test.thy
@@ -9,14 +9,16 @@ program' (demo_logic) prog: \<open>[Set STR ''x'' 5, Guess STR ''y'', Add STR ''
 (* TODO: should be handled by program-command *)
 definition prog where "prog = [Set STR ''x'' 5, Guess STR ''y'', Add STR ''x'' STR ''y'']"
 
-invariant' (demo_logic) start: "\<lambda>m. True"
 
-lemma "hoare start_inv prog (\<lambda>m. m STR ''x'' = 0)"
+lemma "hoare (\<lambda>m. True) prog (\<lambda>m. m STR ''x'' = 0)"
+  (is "hoare ?pre _ ?post")
 proof  -
+
+  invariant (demo_logic) start: ?pre
 
   (* Step 1: Set x 5 *)
 
-  hoare step1: range 0-1 pre start post step1=trivial.
+  hoare step1: range 0-1 pre start post step1 = trivial.
 
   invariant_has step1_x5: step1 \<rightarrow> "\<lambda>m. m STR ''x'' = 5"
     using step1_inv_def by (rule newvalue)
@@ -26,14 +28,10 @@ proof  -
   hoare step2: extends step1 range 1-2 post step2=pick -5.
 
   invariant_has step2_x5: step2 \<rightarrow> "\<lambda>m. m STR ''x'' = 5"
-
-    using step2_inv_def apply (rule unchanged)
-     apply (rule independent_ofI, simp)
-    apply (tactic \<open>Demo_Hoare.use_facts_tac \<^context> @{thms step1_x5} 1\<close>)
-    by simp
+    using step2_inv_def apply (rule unchanged) 
+    using step1_x5 by (auto intro: independent_ofI)
 
   invariant_has step2_y5: step2 \<rightarrow> "\<lambda>m. m STR ''y'' = -5"
-
     using step2_inv_def by (rule newvalue)
 
   (* Step 3: Add x y *)
@@ -42,27 +40,16 @@ proof  -
     by auto
 
   invariant_has step3_y5: step3 \<rightarrow> "\<lambda>m. m STR ''y'' = -5"
-
-    using step3_inv_def apply (rule unchanged)
-      apply (rule independent_ofI, simp)
-     apply simp
-    apply (tactic \<open>Demo_Hoare.use_facts_tac \<^context> @{thms step2_y5} 1\<close>)
-    by simp
+    using step3_inv_def apply (rule unchanged) 
+    using step2_y5 by (auto intro: independent_ofI)
 
   invariant_has step3_x0: step3 \<rightarrow> "\<lambda>m. m STR ''x'' = 0"
+    using step3_inv_def apply (rule wp) 
+    using step2_x5 step2_y5 by (auto simp: pc_imp_def)
 
-    using step3_inv_def apply (rule wp)
-     apply simp
-    apply (tactic \<open>Demo_Hoare.use_facts_tac \<^context> @{thms step2_x5 step2_y5} 1\<close>)
-    by simp
-
-  from step3_valid
+  from step3_valid step3_x0
   show ?thesis
-    unfolding prog_def
-    apply (rule hoare_conseq')
-      apply (simp add: pc_imp_def)
-     apply (rule step3_x0)
-    by simp
+    by (auto intro: hoare_conseq' simp: start_inv_def prog_def)
 
 qed
 
